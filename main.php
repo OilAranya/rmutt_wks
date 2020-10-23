38e19b9665ab2d4d7d53e06cf39897dfb3f7b52f
mo12345 <mochaninan123@gmail.com>
2020-10-23 12:22:25 +0700

    no message

--- main.php
+++ main.php
@@ -1,96 +1,11 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>RMUTTwalkingstreet</title>
-<style type="text/css">
-body {
-	background: #FFFFFF;
-}
-.form-control {
-	box-shadow: none;
-	border-radius: 10px;
-	border-color: #dfe3e8;
-}
-.navbar {
-	color: #fff;
-	background: #343a40 !important;
-	padding: 9px 50px;
-	border: none;
-}
-.navbar .navbar-brand {
-	color: #efe5ff;
-	padding-left: 0;
-	padding-right: 50px;
-	font-size: 25px;
-      
-}
-.navbar .navbar-brand b {
-	color: #FFA500;
-    margin-right: 10px;
-}
-.navbar .navbar-brand i {
-	font-size: 25px;
-	margin-right: 10px;
-	
-}
-.navbar .nav-item i {
-	font-size: 18px;
-    margin-right: 5px;
-}
-.navbar .navbar-nav > a {
-	color: #efe5ff;
-	padding: 9px 15px;
-	font-size: 17px;
-  	margin-right: 40px;
-}
-.row {
-    display: flex;
-    flex-wrap: wrap;
-    margin-right: 50px;
-    margin-left: 50px;
-}
-.btn{
-    margin-right: 0px;
-    padding-bottom: 10px;
-	border-radius: 15px;
-} 
-.btn:hover {opacity: 1}
-h1 {
-	color: #333;
-	font-size: 2rem;
-	text-align: center;
-	font-weight: bold;
-	position: relative;
-}
-h2 {
-	color: #333;
-	font-size: 1.5rem;
-	text-align: center;
-	font-weight: bold;
-	position: relative;
-}
-.card:hover {
-  box-shadow: 0 8px 20px 0 rgba(0,0,0,0.2);
-}
-a:hover {
-  color: yellow;
-}
-.container {
-    padding: 4% 0;
-    z-index: 0;
-}
-p {
-	color: #333;
-	font-weight: bold;
-	position: relative;
-}
-</style>
-</head> 
-<body>
 
+<body>
 <?php include './header.php';?>
  <!-- ----------------content images--------------------------------------------------------------- -->
 <img src="images/logo.gif" alt="" width="100%" height="650">
