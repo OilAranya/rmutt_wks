4ad75e6b6acad9d85e2b6b317e15dcd348646812
Oil-Aranya <aranyathongyoy@gmail.com>
2020-12-23 17:40:05 +0700

    เพิ่มหน้าข้อมูลส่วนตัว

---
+++ user_data.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>RMUTTwalkingstreet</title>
+
+<body>
+<?php include './userp.php';?>
+
+
+<body>
+
+<!-- ---------------------content body---------------------------------------------------------- --> 
+<section class="user_data">
+    <div class="container">
+        <br><br>
+        <div class="page-header">
+            <h1><span class="linehead">ข้อมูลส่วนตัว</span> </h1>
+        </div><br><br>
+        <div class="row row-cols-10 row-cols-md-10">
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="images/ลงทะเบียน.png" class="card-img-top">
+                    <div class="card-body">
+                        <center><h3><a href="#" class="mb-0 text-black">ข้อมูลลงทะเบียน</a></h3></center>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="images/ร้านค้า.jpg" class="card-img-top" >
+                    <div class="card-body">
+                        <center><h3><a href="#" class="mb-0 text-black">ข้อมูลร้านค้า</a></h3></center>
+                    </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                    <img src="images/การจอง.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <center><h3><a href="#" class="mb-0 text-black">ข้อมูลการจอง</a></h3></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<footer>
+</footer>
+
+</body>
+</html>
+
